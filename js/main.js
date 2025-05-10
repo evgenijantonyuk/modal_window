d013ac2989474bc3be1ee3e02d500e1a624a1776
Евгений Антонюк <evgenijantonyuk@gmail.com>
2025-05-10 15:42:03 +0700

    second

--- js/main.js
+++ js/main.js
@@ -1,13 +1,62 @@
-const modalMain = document.querySelector('.modal')
-const modalClose = document.querySelector('.modal__close')
-const modalOpen = document.querySelector('.section__button')
+const modalController = ({ modal, btnOpen, btnClose, time = 300 }) => {
+    const modalMain = document.querySelector(modal)
+    const modalClose = document.querySelector('.modal__close')
+    const modalOpens = document.querySelectorAll(btnOpen)
+    
+    modalMain.style.cssText = `
+display: flex;
+visibility: hidden;
+opacity: 0;
+transition: opacity ${time}ms ease-in-out;
+`
+    
+    const closeModal = (e) => {
+        const target = e.target
+        
+        if (target === modalMain ||
+            (btnClose && target.closest(btnClose)) ||
+            event.code === 'Escape'
+        ) {
+            modalMain.style.opacity = '0'
+            setTimeout(() => {
+                modalMain.style.visibility = 'hidden'
+            }, time)
+            
+            window.removeEventListener('keydown', closeModal)
+        }
+    }
+    
+    const openModal = () => {
+        modalMain.style.visibility = 'visible'
+        modalMain.style.opacity = '1'
+        window.addEventListener('keydown', closeModal)
+    }
+    
+    modalOpens.forEach(btn => {
+        btn.addEventListener('click', openModal)
+    })
+    
+    modalMain.addEventListener('click', closeModal)
+}
 
-modalOpen.addEventListener('click', (event) => {
-    event.preventDefault()
-    modalMain.classList.add('open')
+modalController({
+    modal: '.modal1',
+    btnOpen: '.section__button1',
+    btnClose: '.modal__close',
+    time: 1000
 })
 
-modalClose.addEventListener('click', (event) => {
-    event.preventDefault()
-    modalMain.classList.remove('open')
+modalController({
+    modal: '.modal2',
+    btnOpen: '.section__button2',
+    btnClose: '.modal__close'
 })
+// modalOpen.addEventListener('click', (event) => {
+//     event.preventDefault()
+//     modalMain.classList.add('open')
+// })
+//
+// modalClose.addEventListener('click', (event) => {
+//     event.preventDefault()
+//     modalMain.classList.remove('open')
+// })
